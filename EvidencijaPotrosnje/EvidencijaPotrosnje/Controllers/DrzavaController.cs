f0a1b9feb10a354b4fcaad2fee751e30d7687dc8
ExAmbitionAl <exambition1@gmail.com>
2021-04-26 22:14:39 +0200

    Added DB connection.

---
+++ EvidencijaPotrosnje/EvidencijaPotrosnje/Controllers/DrzavaController.cs
@@ -0,0 +1,20 @@
+﻿using EvidencijaPotrosnje.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EvidencijaPotrosnje.Controllers
+{
+    public class DrzavaController : Controller
+    {
+        // GET: Drzava
+        public ActionResult Index()
+        {
+            ResProbaEntities DB = new ResProbaEntities();
+            List<Drzava> drzave = DB.Drzavas.ToList();
+            return View(drzave);
+        }
+    }
+}
