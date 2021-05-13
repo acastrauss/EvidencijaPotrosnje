8f2ff169d0575c83d49e73ba2247fe9e1e921913
Dusan-Kokotovic <kokotovic999@gmail.com>
2021-05-13 12:50:50 +0200

    Added methods for import

--- EvidencijaPotrosnje/BussinesLogic/ImportData.cs
+++ EvidencijaPotrosnje/BussinesLogic/ImportData.cs
@@ -12,5 +12,21 @@ namespace BussinesLogic
     /// </summary>
     public class ImportData
     {
+
+        List<StateInfoModel> models = new List<StateInfoModel>();
+
+        public static void LoadFromFiles(string weatherFile,string consFile)
+        {
+
+        }
+        private static void LoadWeather(string wf)
+        {
+
+        }
+        private static void LoadConsumption(string cf)
+        {
+
+        }
+
     }
 }
