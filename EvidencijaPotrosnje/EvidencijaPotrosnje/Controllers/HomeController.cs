edbfb4f8f0558d6d717545a8f40ea13bf0581f54
ExAmbitionAl <exambition1@gmail.com>
2021-05-19 17:48:02 +0200

    Working on frontend

--- EvidencijaPotrosnje/EvidencijaPotrosnje/Controllers/HomeController.cs
+++ EvidencijaPotrosnje/EvidencijaPotrosnje/Controllers/HomeController.cs
@@ -14,46 +14,6 @@ namespace EvidencijaPotrosnje.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            StateWeatherModel swm = new StateWeatherModel()
-            {
-                AirTemperature = 12,
-                CloudCover = "aaaa",
-                DevpointTemperature = 34,
-                GustValue = 34,
-                HorizontalVisibility = 44,
-                Humidity = 33,
-                PresentWeather = "aaaa",
-                RecentWeather = "wwww",
-                ReducedPressure = 44,
-                StationPressure = 22,
-                WindDirection = "aaaa",
-                WindSpeed = 44,
-                LocalTime = DateTime.Now
-            };
-
-            StateConsumptionModel scm = new StateConsumptionModel()
-            {
-                CovRatio = 33,
-                DateFrom = DateTime.Now,
-                DateShort = DateTime.Now,
-                DateTo = DateTime.Now,
-                DateUTC = DateTime.Now,
-                StateCode = "aaa",
-                Value = 44,
-                ValueScale = 55
-            };
-
-            StateInfoModel sim = new StateInfoModel();
-            sim.StateConsumption = scm;
-            sim.StateWeather = swm;
-            sim.StateName = "qwerty";
-
-            DBLogic.RemoveAllStates();
-            DBLogic.AddOrUpdateState(sim);
-
-            sim.StateWeather.AirTemperature = 445;
-            DBLogic.AddOrUpdateState(sim);
-
             return View();
         }
     }
