df9267af9b1b84368ea8d0a7c66a4a103bb5b688
Dusan-Kokotovic <kokotovic999@gmail.com>
2021-05-28 14:31:33 +0200

    Ispravljanje inputa

--- EvidencijaPotrosnje/BussinesLogic/ImportData.cs
+++ EvidencijaPotrosnje/BussinesLogic/ImportData.cs
@@ -34,28 +34,33 @@ namespace BussinesLogic
                 {
                     StateWeatherModel swm = new StateWeatherModel();
                     string[] fields = csvParser.ReadFields();
-                    if (fields[0].StartsWith("#"))
+                    if (fields[0].StartsWith("#") || fields[0].Contains("Local"))
                     {
                         continue;
                     }
                     // OVDE NEDOSTAJE POLJE LOKALNO VREME IZ TABELE WEATHER,ONO CE NAM KASNIJE TREBATI ZA PRIKAZ
-                    swm.LocalTime = DateTime.Parse(fields[0]);
-                    swm.AirTemperature = float.Parse(fields[1]);
-                    swm.StationPressure = float.Parse(fields[2]);
-                    swm.ReducedPressure = float.Parse(fields[3]);
-                    swm.Humidity = int.Parse(fields[4]);
+                    swm.LocalTime = DateTime.ParseExact(fields[0], "dd.MM.yyyy HH:mm", null);
+                    float airTemp, stationPressure, reducedPressure;
+                    swm.AirTemperature = float.TryParse(fields[1], out airTemp) ? airTemp : 0;
+                    swm.StationPressure = float.TryParse(fields[2], out stationPressure) ? stationPressure : 0;
+                    swm.ReducedPressure = float.TryParse(fields[3], out reducedPressure) ? reducedPressure : 0;
+                    int humidity;
+                    swm.Humidity = int.TryParse(fields[4], out humidity) ? humidity : 0;
                     swm.WindDirection = fields[5];
-                    swm.WindSpeed = int.Parse(fields[6]);
-                    swm.GustValue = int.Parse(fields[7]);
+                    int windSpeed;
+                    swm.WindSpeed = int.TryParse(fields[6], out windSpeed) ? windSpeed : 0;
+                    swm.GustValue = fields[7] == "" ? 0 : int.Parse(fields[7]);
                     swm.PresentWeather = fields[8];
                     swm.RecentWeather = fields[9];
                     swm.CloudCover = fields[10];
-                    swm.HorizontalVisibility = int.Parse(fields[11]);
-                    swm.DevpointTemperature = int.Parse(fields[12]);
+                    int horizontalVisibility;
+                    swm.HorizontalVisibility = int.TryParse(fields[11],out horizontalVisibility) ? horizontalVisibility : 0;
+                    int devTemp;
+                    swm.DevpointTemperature = int.TryParse(fields[12], out devTemp) ? devTemp : 0;
 
                     DataKeys keys = new DataKeys()
                     {
-                        Name = DBLogic.GetFullStateName(state),
+                        Name = DBLogic.GetShortStateName(state),
                         EndDate = swm.LocalTime,
                         StartDate = DateTime.MinValue
                     };
@@ -92,20 +97,23 @@ namespace BussinesLogic
                     StateConsumptionModel scm = new StateConsumptionModel();
                     string[] fields = csvParser.ReadFields();
                     
-                    scm.DateUTC = DateTime.Parse(fields[1]);
-                    scm.DateShort = DateTime.Parse(fields[2]);
-                    
+                    scm.DateUTC = new DateTime();
+                    scm.DateShort = DateTime.ParseExact(fields[2], "M/d/yyyy", null);
+
                     // the next 2 is just hours
-                    scm.DateFrom = DateTime.Parse(fields[3]);
-                    scm.DateTo = DateTime.Parse(fields[4]);
+                    scm.DateFrom = DateTime.ParseExact(fields[3], "H:mm", null);
+                    scm.DateTo = DateTime.ParseExact(fields[4], "H:mm", null);
 
                     scm.StateCode = fields[5];
-                    scm.CovRatio = int.Parse(fields[6]);
-                    scm.Value = double.Parse(fields[7]);
-                    scm.ValueScale = double.Parse(fields[8]);
+                    int covRatio;
+                    scm.CovRatio = int.TryParse(fields[6], out covRatio) ?covRatio : 0;
+                    double value;
+                    scm.Value = double.TryParse(fields[7], out value) ? value : 0;
+                    double valueScale;
+                    scm.ValueScale = double.TryParse(fields[8], out valueScale) ? valueScale : 0;
 
                     // if it is the right country and date
-                    if(scm.StateCode == DBLogic.GetShortStateName(stateName) && startDate < scm.DateShort && endDate > scm.DateShort) 
+                    if (scm.StateCode == DBLogic.GetShortStateName(stateName) && startDate < scm.DateShort && endDate > scm.DateShort) 
                     {
                         DataKeys keys = new DataKeys()
                         {
--- EvidencijaPotrosnje/EvidencijaPotrosnje/Controllers/HomeController.cs
+++ EvidencijaPotrosnje/EvidencijaPotrosnje/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using System.Data.Entity;
 using BussinesLogic;
 using SharedModels;
+using SharedModels.HelperClasses;
+using System.Web.Hosting;
 
 namespace EvidencijaPotrosnje.Controllers
 {
@@ -14,7 +16,14 @@ namespace EvidencijaPotrosnje.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            
+            ImportParameters parameters = new ImportParameters(HostingEnvironment.MapPath("~/App_Data/WeatherData/Weather-Serbia.csv"), HostingEnvironment.MapPath("~/App_Data/ConsumptionData/Consumption.csv"), "Serbia", DateTime.MinValue, DateTime.MaxValue);
+
+            ImportData.Load(parameters);
+
+
+
+            return View(CurrentData.Data);
         }
     }
 }
